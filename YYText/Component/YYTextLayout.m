ac198c90ba48df1ca86756dcc438896e770cf9fa
Robert-Up <robert_ding_up@163.com>
2026-05-07 10:14:50 +0800

    Update YYTextLayout.m

--- YYText/Component/YYTextLayout.m
+++ YYText/Component/YYTextLayout.m
@@ -1504,19 +1504,19 @@ fail:
     }
     
     [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
+          if (isVertical) {
+              position = (fabs(left - point.y) < fabs(right - point.y)) ? prev : next;
+          } else {
+              position = (fabs(left - point.x) < fabs(right - point.x)) ? prev : next;
+          }
     }];
     
     [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
+         if (isVertical) {
+              position = (fabs(left - point.y) < fabs(right - point.y)) ? prev : next;
+          } else {
+              position = (fabs(left - point.x) < fabs(right - point.x)) ? prev : next;
+          }
     }];
     
     if (position < _visibleRange.location) position = _visibleRange.location;
